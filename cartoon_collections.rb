fbcb0725c9be334614eee480fc7859e9601f4e4d
Will Bush <williamdcbush@gmail.com>
2019-07-06 10:44:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_call(# code an argument here
   # Your code here
 end
 

7519732ff8e9a52028472cc518e0d77e3fcd4c24
Will Bush <williamdcbush@gmail.com>
2019-07-06 10:24:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
 def roll_call_dwarves(dwarves)
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   puts dwarves.each_with_index 
+  
+  a=[11,22,31,224,44].each_with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
+  
 end
 
 def summon_captain_planet# code an argument here

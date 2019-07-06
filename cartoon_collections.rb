5634b9af508d8ee64bc4c00af07f0123cfa46c44
Will Bush <williamdcbush@gmail.com>
2019-07-06 11:00:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(assorted_words)
-  assorted_words.each {|word|
+  assorted_words.each { |word|
   if call.length < 4
     answer = false
   else
